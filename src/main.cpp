8849da410bb9018ec6f8bf265c97fa7d0fad6369
nanov <mitko.n@gmail.com>
2024-11-26 23:32:41 +0200

    chore: better build, code celanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <ostream>
+#include <string>
 #include <string_view>
+#include <unordered_set>
+
+
+const auto built_in_commands = std::unordered_set<std::string_view>{"echo", "exit", "type"};
 
 int main() {
 	int exit_code = -1;
@@ -18,11 +23,19 @@ int main() {
 		std::getline(std::cin, input);
 		if (input == "exit 0")
 			return 0;
-		if (input.rfind("echo", 0) == 0) {
+		if (input.rfind("echo ", 0) == 0) {
 			std::cout << std::string_view(input).substr(5) << std::endl;
 			continue;
 		} 
-
+		if (input.rfind("type ") == 0) {
+			auto l = std::string_view(input).substr(5);
+			if (built_in_commands.contains(l)) {
+				std::cout << l << " is a shell builtin" << std::endl;
+			} else {
+				std::cout << l << ": not found" << std::endl;
+			}
+			continue;
+		}
 	
 		std::cout << input << ": command not found" << std::endl;
 	}
