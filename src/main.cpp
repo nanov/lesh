2e4c6006e2bb7bd93b1e180c5dd16065ccfdb788
nanov <mitko.n@gmail.com>
2024-11-26 22:52:30 +0200

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,17 @@ int main() {
   std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
-  std::cout << "$ ";
 
   std::string input;
-  std::getline(std::cin, input);
 
-	std::cout << input << ": command not found" << std::endl;
+
+	while(true) {
+		std::cout << "$ ";
+		std::getline(std::cin, input);
+		if (input == "exit")
+			break;
+		std::cout << input << ": command not found" << std::endl;
+	}
+	
+	
 }
