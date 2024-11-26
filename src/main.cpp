82a19b887b1f16ba2d0a43a68662069c4ac3ae49
nanov <mitko.n@gmail.com>
2024-11-26 22:44:16 +0200

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ostream>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -10,4 +11,6 @@ int main() {
 
   std::string input;
   std::getline(std::cin, input);
+
+	std::cout << input << " : command not found" << std::endl;
 }
