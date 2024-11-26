20ba33014898f604360abbee18301253e4abf6a7
nanov <mitko.n@gmail.com>
2024-11-26 23:10:09 +0200

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ostream>
+#include <string_view>
 
 int main() {
 	int exit_code = -1;
@@ -17,10 +18,16 @@ int main() {
 		std::getline(std::cin, input);
 		if (input == "exit 0")
 			return 0;
+		if (input.rfind("echo", 0) == 0) {
+			std::cout << std::string_view(input).substr(5) << std::endl;
+			continue;
+		} 
+
 	
 		std::cout << input << ": command not found" << std::endl;
-
 	}
+
+
 
 	return exit_code; 
 	
