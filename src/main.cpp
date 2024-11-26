c2f63354415c0856d5ab89396b3f9570fde43575
nanov <mitko.n@gmail.com>
2024-11-26 22:59:38 +0200

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,26 @@
 #include <ostream>
 
 int main() {
+	int exit_code = -1;
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
 
-  std::string input;
+  std::string input = "";
 
 
 	while(true) {
 		std::cout << "$ ";
 		std::getline(std::cin, input);
-		if (input == "exit")
-			break;
+		if (input == "exit 0")
+			return 0;
+	
 		std::cout << input << ": command not found" << std::endl;
 
 	}
-	
+
+	return exit_code; 
 	
 }
